b576b34be6d769d65d02ef3e02b72beba2ce684b
DENISTAVARES <DENISBRAGATAVARES@GMAIL.COM>
2025-01-23 08:36:18 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/application/dtos/CriarPessoaRequest.java
@@ -0,0 +1,8 @@
+package br.com.cotiinformatica.application.dtos;
+import lombok.Data;
+@Data
+public class CriarPessoaRequest {
+	private String nome;
+	private String email;
+	private String senha;
+}
