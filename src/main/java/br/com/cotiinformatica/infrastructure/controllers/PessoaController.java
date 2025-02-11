ce5757b535972b853e3cd1cc4c5cd6655d941db4
DENISTAVARES <DENISBRAGATAVARES@GMAIL.COM>
2025-02-11 07:43:53 -0300

    Alterações até aula 20

--- src/main/java/br/com/cotiinformatica/infrastructure/controllers/PessoaController.java
+++ src/main/java/br/com/cotiinformatica/infrastructure/controllers/PessoaController.java
@@ -1,4 +1,6 @@
 package br.com.cotiinformatica.infrastructure.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -6,12 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.cotiinformatica.application.dtos.CriarPessoaRequest;
 import br.com.cotiinformatica.application.dtos.CriarPessoaResponse;
+import br.com.cotiinformatica.application.ports.in.PessoaService;
 @RestController
 @RequestMapping("/api/v1/pessoas")
 public class PessoaController {
+	@Autowired PessoaService pessoaService;
+	
 	@PostMapping("cadastrar")
 	public ResponseEntity<CriarPessoaResponse> cadastrar(@RequestBody CriarPessoaRequest request) {
-		// TODO
-		return null;
+		var response = pessoaService.criar(request);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 }
+
+
